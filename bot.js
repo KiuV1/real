a24fce6353426f70e666f58e53c88961793dc88b
KiuV1 <45211476+KiuV1@users.noreply.github.com>
2018-12-14 13:54:04 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,41 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "d";
+var adminprefix = '.'
+const developers = ["الاي دي حقك هنا"]
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+     
+  if (message.content.startsWith(adminprefix + 'setg')) {
+    client.user.setGame(argresult);
+      message.channel.send(`:white_check_mark: Changing The Playing Mode To **${argresult}** :white_check_mark:  `)
+  } else
+     if (message.content === (adminprefix + "leave")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(adminprefix + 'setw')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`:white_check_mark: Successfully Changing The Watching Mode To **${argresult}** :white_check_mark: `)
+  } else
+  if (message.content.startsWith(adminprefix + 'setl')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`:white_check_mark: Successfully Changing The Listening Mode To **${argresult}** :white_check_mark: `)
+  } else
+  if (message.content.startsWith(adminprefix + 'sets')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/One");
+      message.channel.send(`:white_check_mark: Successfully Changing The Streaming Mode To **${argresult}** :white_check_mark: `)
+  }
+  if (message.content.startsWith(adminprefix + 'setn')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith(adminprefix + 'seta')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
+});
+ 
+ 
+ 
+client.login(process.env.BOT_TOKEN);//
